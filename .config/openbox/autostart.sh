a5cd9f701cc0f05f7cccfd04162c5a6daef3889d
Mikael Roos <mikael.t.h.roos@gmail.com>
2015-07-04 14:11:43 +0200

    openbox running

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -1,3 +1,5 @@
-(sleep 3 && pypanel) &
-hsetroot -solid "#202070" &
+#(sleep 3 && pypanel) &
+#tint2 &
+$HOME/bin/monitor.sh desktop &
+hsetroot -solid "#202020" &
 xscreensaver -no-splash &
--- bin/monitor.sh
+++ bin/monitor.sh
@@ -8,7 +8,8 @@ elif [ "$1" = 'dual' ]; then
   xrandr --output DVI-D-1 --auto --primary --output DVI-I-1 --auto --pos 2560x400
 elif [ "$1" = 'tre' ]; then
   xrandr --output DVI-D-1 --auto --primary --pos 1920x0 --output DVI-I-1 --auto --pos 4480x400 --output HDMI-1 --auto --pos 0x400
+elif [ "$1" = 'desktop' ]; then
+  xrandr --output DVI-D-1 --auto --primary --pos 1920x0 --output DVI-I-1 --auto --pos 4480x0 --output HDMI-1 --auto --pos 0x0
 elif [ "$1" = 'triple27' ]; then
   xrandr --output DP1 --auto --primary --pos 1920x0 --output DP2 --auto --pos 3840x0 --output VGA1 --auto --pos 0x0
 fi
-
